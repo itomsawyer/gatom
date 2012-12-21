735a9f2832583db0e22908a0faa50d6db555a26c
itomsawyer <tomsawyer126@gmail.com>
2012-12-20 23:38:31 -0500

    finished walking of the src path and copy to dst path

--- gatom.py
+++ gatom.py
@@ -1,6 +1,8 @@
 import argparse
 from jinja2 import  PackageLoader , Environment
+from PIL import Image
 import sys,os
+import imghdr
 
 sys.path.append(os.path.join(os.path.dirname(__file__),os.path.pardir))
 
@@ -13,12 +15,14 @@ print template.render()
 
 class album:
     name = 'gallery'
-    path = ''
+    url = '/gallery'
     cover = ''
     totalPhoto = 0
     totalSubAlbum = 0
-    subAlbum = list()
-    photo = list()
+
+class photo:
+    name = 'photo'
+    url = '/'
     
 
 def runHttpServer(documentRoot,port):
@@ -30,9 +34,69 @@ def runHttpServer(documentRoot,port):
 
     httpd.serve_forever()
 
-def analyseGalleryStruct(srcPath):
+def prepare_image(src,dst):
+    im = Image.open(src)
+    im.save(dst)
+    pass
+
+def generator_html(dst ,name ,images,albums):
+    print images
+    print albums
+    pass
+
+
+def createAlbums(srcPath,dstPath,urlPath):
+    images_list = list()
+    albums_list = list()
+    album_item = album()
+
+    album_item.name =os.path.basename(dstPath)
+    album_item.url = urlPath
+    print album_item.url
+
+    for obj in  os.listdir(srcPath):
+        src_obj_path = os.path.join(srcPath,obj)
+        dst_obj_path = os.path.join(dstPath,obj)
+        
+        if os.path.isfile(src_obj_path) and ( imghdr.what(src_obj_path) is not None ):
+            image = photo()
+            album_item.totalPhoto +=1
+            if album_item.cover is '':
+                album_item.cover = image.url 
+
+            prepare_image(src_obj_path,dst_obj_path)
+
+            image.name = obj
+            image.url = urlPath + '/' +obj
+            images_list.append(image)
+
+        if os.path.isdir(src_obj_path):
+            if not os.path.exists(dst_obj_path):
+                os.mkdir(dst_obj_path)
+            album_item.totalSubAlbum += 1
+            albums_list.append(createAlbums(src_obj_path,os.path.join(dstPath,obj),os.path.join(urlPath,obj)) )
+
+
+    """if albums_list or  image_list is not none"""
+    generator_html(dstPath,album_item.name ,images_list,albums_list)
+    return album
+            
+
+def buildGallery(srcPath,dstPath):
+    if not os.path.exists(dstPath):
+        os.mkdir(dstPath)
+    urlPath = '/'
+
+    if not os.path.exists(dstPath):
+        print dstPath
+        os.mkdir(dstPath)
+    createAlbums(srcPath,dstPath,os.path.join(urlPath,os.path.basename(dstPath)))
+
     
-    print os.walk(srcPath).
+
+            
+
+    
 
 def process(args):
     "Initialize arguments"
@@ -43,7 +107,7 @@ def process(args):
     else:
         serverPort = 8012
     """TODO create menu architecture """
-    gallery = analyseGalleryStruct(src_gallery_path)
+    gallery = buildGallery(src_gallery_path,dst_gallery_path)
     """TODO prepare photoes """
     """TODO create gallery"""
 
